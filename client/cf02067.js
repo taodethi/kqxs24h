6d5e16420296c16c3c8f86d61cd35495d3c2eb0c
taodethi <87457500+taodethi@users.noreply.github.com>
2022-11-23 23:39:51 +0700

    Delete client directory

--- client/cf02067.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[33],{416:function(n,t,e){"use strict";e.r(t);var l=e(2),component=Object(l.a)({},(function(){return(0,this._self._c)("datagrid")}),[],!1,null,null,null);t.default=component.exports}}]);
